e64d745c7d75351c6206771e3c8bb4a435efef55
lolmageap <ekxk1234@naver.com>
2025-01-28 23:08:36 +0900

    refactor routing and serialization setup; add OpenAPI documentation

--- src/main/kotlin/Serialization.kt
+++ src/main/kotlin/Serialization.kt
@@ -1,24 +1,9 @@
 package com.example
 
-import com.fasterxml.jackson.databind.*
-import io.ktor.http.*
+import com.fasterxml.jackson.databind.SerializationFeature
 import io.ktor.serialization.jackson.*
-import io.ktor.serialization.kotlinx.json.*
 import io.ktor.server.application.*
-import io.ktor.server.plugins.calllogging.*
 import io.ktor.server.plugins.contentnegotiation.*
-import io.ktor.server.plugins.doublereceive.*
-import io.ktor.server.plugins.requestvalidation.RequestValidation
-import io.ktor.server.plugins.requestvalidation.ValidationResult
-import io.ktor.server.plugins.swagger.*
-import io.ktor.server.request.*
-import io.ktor.server.response.*
-import io.ktor.server.routing.*
-import org.jetbrains.exposed.sql.*
-import org.koin.dsl.module
-import org.koin.ktor.plugin.Koin
-import org.koin.logger.slf4jLogger
-import org.slf4j.event.*
 
 fun Application.configureSerialization() {
     install(ContentNegotiation) {
