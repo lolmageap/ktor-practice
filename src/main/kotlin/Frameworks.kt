8b4589ab93f9580609aacc98e867b7376a662c56
lolmageap <ekxk1234@naver.com>
2025-01-30 22:06:06 +0900

    add Koin dependency injection setup with user module configuration

--- src/main/kotlin/Frameworks.kt
+++ src/main/kotlin/Frameworks.kt
@@ -1,12 +1,31 @@
 package com.example
 
 import io.ktor.server.application.*
+import org.koin.core.module.dsl.createdAtStart
+import org.koin.core.module.dsl.named
+import org.koin.core.module.dsl.withOptions
+import org.koin.dsl.module
 import org.koin.ktor.plugin.Koin
 import org.koin.logger.slf4jLogger
 
 fun Application.configureFrameworks() {
     install(Koin) {
         slf4jLogger()
-        modules()
+        modules(
+            userModule,
+        )
+    }
+}
+
+val userModule = module {
+    single { } withOptions {
+        // named function is qualifier for dependency injection
+        named("singleton")
+        createdAtStart()
+    }
+
+    factory { } withOptions {
+        named("alwaysNew")
+        createdAtStart()
     }
 }
