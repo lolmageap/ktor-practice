db884656f3738be11e8b6d42959c96a89fd0295a
lolmageap <ekxk1234@naver.com>
2025-02-09 17:14:29 +0900

    refactor: remove unused user data classes and clean up routing configuration

--- src/main/kotlin/Routing.kt
+++ src/main/kotlin/Routing.kt
@@ -4,7 +4,6 @@ import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.plugins.statuspages.*
 import io.ktor.server.response.*
-import java.time.LocalDate
 
 fun Application.configureRouting() {
     install(StatusPages) {
@@ -13,19 +12,3 @@ fun Application.configureRouting() {
         }
     }
 }
-
-@JvmInline
-value class UserName(val value: String)
-
-data class CreateUserRequest(
-    val name: UserName,
-    val age: Int,
-    val birthday: LocalDate,
-)
-
-data class UserResponse(
-    val id: Long,
-    val name: UserName,
-    val age: Int,
-    val birthday: LocalDate,
-)
