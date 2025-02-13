aa3248777f9559d7920439201c6552244ee63431
lolmageap <ekxk1234@naver.com>
2025-02-13 22:11:22 +0900

    refactor Application.kt to remove unused module function and streamline main entry point

--- src/main/kotlin/Application.kt
+++ src/main/kotlin/Application.kt
@@ -1,15 +1,5 @@
 package com.example
 
-import io.ktor.server.application.*
-
 fun main(args: Array<String>) {
     io.ktor.server.cio.EngineMain.main(args)
-}
-
-fun Application.module() {
-    configureMonitoring()
-    configureSerialization()
-    configureDatabases()
-    configureFrameworks()
-    configureRouting()
 }
